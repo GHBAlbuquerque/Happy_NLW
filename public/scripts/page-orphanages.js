c67edaf34425a60f5fff568abca2944a9dc2363d
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2020-10-15 12:26:07 -0300

    page maps done

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,34 @@
+// create-map
+
+const map = L.map('mapid').setView([-23.5339705, -46.8544877], 16); //coordinates & zoom
+
+
+// tile-layer
+
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+).addTo(map);
+
+//create icon
+
+var icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize:     [58, 68], // size of the icon
+    iconAnchor:   [29, 68], // point of the icon which will correspond to marker's location
+    popupAnchor:  [170, 2] // point from which the popup should open relative to the iconAnchor
+});
+
+//create popup overlay
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanages.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+// pop-ups
+
+L.marker([-23.5339705, -46.8544877], { icon }) //destructor icon const to icon
+    .addTo(map)
+    .bindPopup(popup);
