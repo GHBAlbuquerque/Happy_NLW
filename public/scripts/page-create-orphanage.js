69be965c8bd8f14f2f20f2810e5f00a8a5cd0aff
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2020-10-16 16:14:52 -0300

    ajustes finais aula3

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,4 +1,4 @@
-// create-map
+// ***************** MAP ****************
 
 const map = L.map('mapid').setView([-23.5339705, -46.8544877], 16); //coordinates & zoom
 
@@ -20,6 +20,9 @@ let marker;
 
 //create and add marker
 map.on('click', function(event) {
+    const formLat = document.querySelector("#latitude")
+    const formLng = document.querySelector("#longitude")
+
     let latitude = event.latlng.lat;
     let longitude = event.latlng.lng;
 
@@ -31,4 +34,73 @@ map.on('click', function(event) {
     
     //add icon layer
     marker = L.marker([latitude, longitude], { icon }).addTo(map);
+    formLat.value = latitude;
+    formLng.value = longitude;
 })
+
+
+
+// ***************** ADD PHOTO ****************
+
+    //get photo container #images
+const container = document.querySelector("#images")
+
+const divMsg = document.createElement('div');
+divMsg.classList.add('msg', 'msg-erro');
+
+
+function addPhotoField() {
+    //get field .new-upload
+    let fieldsContainer = document.querySelectorAll(".new-upload")
+
+    //clone last added img
+    const newFieldContainer = fieldsContainer[fieldsContainer.length-1].cloneNode(true)
+
+    //verify if input is empty
+    let inputValue = newFieldContainer.children[0].value
+    if (inputValue == "") {
+        divMsg.textContent = 'Digite uma URL!';
+        container.insertAdjacentElement('beforebegin', divMsg);
+    } else {
+    //clean before cloning
+    newFieldContainer.children[0].value = ""
+
+    //add clone to container
+    if (document.querySelector(".msg") != null) {
+        document.querySelector(".msg").remove()
+    }
+
+    container.appendChild(newFieldContainer)
+    }
+
+}
+
+// remove "add photo field"
+container.addEventListener("click", function(event) {
+    //get field .new-upload
+    let fieldsContainer = document.querySelectorAll(".new-upload")
+
+    if (event.target.tagName == 'IMG' || event.target.tagName == 'SPAN') {
+        if (fieldsContainer.length != 1) {
+            const cancelButton = event.target
+            cancelButton.closest('div').remove()
+        } else {
+            fieldsContainer[0].children[0].value=""
+        }
+    }
+})
+
+
+// ***************** ADD PHOTO ****************
+
+function toggleSelect(event) {
+    //remover classe active do outro
+    document.querySelector(".button-select .active").classList.remove("active")
+
+    //pegar o botão clicado e adc classe
+    let clickedButton = event.target
+    clickedButton.classList.add("active")
+
+    //atualizar o input hidden com o valor selecionado
+    document.querySelector("#open-on-weekends").value = clickedButton.dataset.value
+}
